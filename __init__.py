759f4896da282f2347265582b278f9e6ea5cdcea
Andreas Lorensen <andlo@outlook.dk>
2018-10-30 17:45:58 +0000

    test

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft.audio import wait_while_speaking
+import io
 
 class TellAStory(MycroftSkill):
     def __init__(self):
@@ -7,8 +8,13 @@ class TellAStory(MycroftSkill):
 
     @intent_file_handler('story.a.tell.intent')
     def handle_story_a_tell(self, message):
-        self.speak_dialog('story.a.tell')
-
+        #self.speak_dialog('story.a.tell', 'r')
+        AppPath = self._dir
+        filename = AppPath + "/stories/the_little_match_girl_andersen.txt"
+        with io.open(filename) as f:
+            for line in f:
+                wait_while_speaking()
+                self.speak(line)
 
 def create_skill():
     return TellAStory()
